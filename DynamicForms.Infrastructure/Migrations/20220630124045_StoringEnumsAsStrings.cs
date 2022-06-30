35be0b065797ee8108af0023fcb019d021f8bf75
Ali Shah <Ali.Shah@nottscc.gov.uk>
2022-06-30 13:44:07 +0100

    StoringEnumsAsStrings

---
+++ DynamicForms.Infrastructure/Migrations/20220630124045_StoringEnumsAsStrings.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DynamicForms.Infrastructure.Migrations
+{
+    public partial class StoringEnumsAsStrings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "QuestionTypes",
+                type: "nvarchar(24)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayType",
+                table: "QuestionTypes",
+                type: "nvarchar(24)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "QuestionTypes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(24)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DisplayType",
+                table: "QuestionTypes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(24)");
+        }
+    }
+}
